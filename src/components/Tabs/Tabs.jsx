af51b3637242db9ee014c3cd614b5c83ffe9e35b
Змеев Илья <zmeev.i.v@edu.mirea.ru>
2025-10-06 22:09:49 +0300

    bug fixed

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,15 +1,15 @@
-import { useEffect, useState } from 'react';
+//import { useEffect, useState } from 'react';
 import React from "react";
 import "./Tabs.css";
 
 export default function Tabs({ value, onChange }) {
-  const ADMIN_IDS = process.env.ADMIN_IDS.split(',').map(id => parseInt(id, 10));
-  const [userId, setUserId] = useState(null);
+  //const ADMIN_IDS = process.env.ADMIN_IDS.split(',').map(id => parseInt(id, 10));
+  //const [userId, setUserId] = useState(null);
 
-  useEffect(() => {
-    const tg = window.Telegram?.WebApp;
-    setUserId(tg?.initDataUnsafe?.user?.id ?? null);
-  }, []);
+  // useEffect(() => {
+  //   const tg = window.Telegram?.WebApp;
+  //   setUserId(tg?.initDataUnsafe?.user?.id ?? null);
+  // }, []);
 
   return (
     <div className="tabs">
@@ -19,13 +19,13 @@ export default function Tabs({ value, onChange }) {
       >
         Магазин
       </button>
-      {ADMIN_IDS.include(userId) &&
-        (<button
-          className={value === "admin" ? "tab active" : "tab"}
-          onClick={() => onChange("admin")}
-        >
-          Админ
-      </button>)}
+
+      <button
+        className={value === "admin" ? "tab active" : "tab"}
+        onClick={() => onChange("admin")}
+      >
+        Админ
+      </button>
     </div>
   );
 }
