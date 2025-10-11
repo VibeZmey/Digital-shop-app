eb3d5a4619a72d0a18ea3ad4eccf61030fbba5ae
Змеев Илья <zmeev.i.v@edu.mirea.ru>
2025-10-11 22:19:15 +0300

    test

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import "./Header.css";
 export default function Header() {
   return (
     <header className="app-header">
-      <img src="/icons/logo.png" alt="logo" />
+      <img className="logo" src="/icons/logo.png" alt="logo" />
       <div className="brand">
         <div className="brand-title">DigitalShop</div>
         <div className="brand-sub">цифровые подписки</div>
