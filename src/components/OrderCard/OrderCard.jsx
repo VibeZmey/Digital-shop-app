191822a12924a5bf6def4bfb5e40bd0bb9ee40d5
Змеев Илья <zmeev.i.v@edu.mirea.ru>
2025-10-15 17:14:22 +0300

    test

--- src/components/OrderCard/OrderCard.jsx
+++ src/components/OrderCard/OrderCard.jsx
@@ -7,7 +7,7 @@ export default function OrderCard ({ order, onClick }) {
       <div className="order-card__left">
         <img className="order-card__image" src={order.image} alt={order.name} />
         <div className="order-card__meta">
-          <div className="order-card__name">{name}</div>
+          <div className="order-card__name">{order.name}</div>
           {order.price!==0 ? <div className="order-card__price">{order.price} ₽</div> : null}
         </div>
       </div>
